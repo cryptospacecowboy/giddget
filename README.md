a54dc47098d5dc769138ff0b8f934c1e918756ab
Karl Monson <karlmonson@Karls-Mac-mini.local>
2022-04-28 19:50:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Giddget
+A Giddy Price Widget for iOS
+
+You can find the setup guide on [GiveSendGiddy.com](https://www.givesendgiddy.com/giddy-widget). 
+
+Learn more about Giddy DeFi by visiting their [official site](https://giddy.co), and [join the waitlist](app.giddy.co/share/sc1enL7U) for the mobile app coming soon!
+
+![Giddget](https://repository-images.githubusercontent.com/486780309/2783dc4e-0bb7-4e13-a379-54988a9a64ad "Giddget")
+
+## Authors
+
+- Space Cowboy (Tip Jar: 0x7E217ed7c2b03ec4bb163341875fd7cA5e4c726e)
+- TommyTippie (Tip Jar: 0x8b776F21aD7389Ce1c37913d696c2Eb778771c1F)
+
+## License
+
+The Giddget is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ giddget.js
@@ -0,0 +1,56 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: cyan; icon-glyph: magic;
+// ----------------------------------------------------------
+// The Giddget - A Giddy Price Widget for iOS
+// Space Cowboy's Tip Jar: 0x7E217ed7c2b03ec4bb163341875fd7cA5e4c726e
+// TommyTippie's Tip Jar: 0x8b776F21aD7389Ce1c37913d696c2Eb778771c1F
+// ----------------------------------------------------------
+
+// Theme Options - 'light' or 'dark'
+const theme = 'light'
+const bgColor = (theme == 'light') ? Color.white() : Color.black()
+const textColor = (theme == 'light') ? Color.black() : Color.white()
+const logo = (theme == 'light') ? 'https://i.ibb.co/S5W7pQC/giddy-logo-horizontal.png' : 'https://i.ibb.co/2WLSW3M/giddy-logo-horizontal-white.png'
+
+// Get Current Price
+const url = 'https://us-central1-fifth-liberty-317301.cloudfunctions.net/GetGiddyPrice'
+const req = new Request(url)
+const price = await req.loadString()
+
+// Build Widget
+let widget = new ListWidget()
+widget.useDefaultPadding()
+widget.backgroundColor = bgColor
+
+let titleText = widget.addText('GIDDY COIN')
+titleText.textColor = textColor
+titleText.font = Font.semiboldMonospacedSystemFont(10)
+titleText.textOpacity = 0.5
+titleText.centerAlignText()
+
+widget.addSpacer(25)
+
+let alertText = widget.addText('$'+Number(price).toFixed(4))
+alertText.textColor = textColor
+alertText.font = Font.boldMonospacedSystemFont(24)
+alertText.centerAlignText()
+
+let alertTitle = widget.addText("LAST PRICE")
+alertTitle.textColor = textColor
+alertTitle.font = Font.semiboldMonospacedSystemFont(10)
+alertTitle.textOpacity = 0.5
+alertTitle.centerAlignText()
+
+widget.addSpacer(25)
+
+let imgReq = new Request(logo)
+let image = await imgReq.loadImage()
+let widgetImage = widget.addImage(image)
+widgetImage.imageSize = new Size(70,15)
+widgetImage.centerAlignImage()
+
+// Display Widget
+widget.presentSmall()
+Script.setWidget(widget)
+Script.complete()
